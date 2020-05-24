cab661d47be96f2255cd95cde6b132e655531807
m1ndfulf0x <59140415+m1ndfulf0x@users.noreply.github.com>
2020-05-24 18:10:54 +0200

    Update minimal fast bop.rb

--- minimal fast bop.rb
+++ minimal fast bop.rb
@@ -2,7 +2,7 @@
 
 define :e3_tick do
   live_loop :arp do
-    play (scale :e3, :minor_pentatonic).tick, release: 0.1, amp: 0.5
+    play (scale :e3, :minor_pentatonic).tick, release: 0.2, amp: 0.6
     sleep 0.125
   end
 end
@@ -14,5 +14,25 @@ define :e2_tick do
   end
 end
 
+define :one_note do |n, s, a|
+  play n, amp: a
+  sleep s
+end
+
+define :drum1 do |s|
+  live_loop :splash do
+    sleep s
+    sample :drum_cymbal_pedal, amp: 0.8
+    sleep 0.22
+  end
+end
+
+
 e3_tick
 e2_tick
+drum1 0.53
+live_loop :def_note do
+  one_note 64, 4, 1.2
+  one_note 59, 3, 1.2
+end
+
