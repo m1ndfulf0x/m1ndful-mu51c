58842a9d135adcee6cb13c4c7ccab159d3e12dad
m1ndfulf0x <59140415+m1ndfulf0x@users.noreply.github.com>
2020-07-04 19:23:49 +0200

    add slicer effect

--- terra-jingle.rb
+++ terra-jingle.rb
@@ -1,4 +1,9 @@
 use_bpm 155
+with_fx :slicer, phase: 0.25, phase_slide: 5 do |e|
+  sample :ambi_glass_hum, amp: 4.0, rate: 2.0, attack: 0.1 , sustain: 2.0, release: 1.2
+  control e, phase: 0.125
+  sleep 1.0
+end
 use_synth :fm
 play :B3, pan: -0.5
 use_synth :hoover
@@ -21,7 +26,7 @@ play :Gs5, pan: 0.2
 sleep 0.5
 play :E4, pan: 0.4
 sleep 0.5
-play :B3, attack: 0.1, attack_level: 0.1, decay: 0.2, decay_level: 1, sustain: 1.1, sustain_level: 0.8, release: 1.5, pan: 0.5
-use_synth :hoover
-play :Fs4, amp: 0.8, cutoff: 90, attack: 0.1, attack_level: 0.1, sustain: 0.4, sustain_level: 1.1, pan: 0.5
+play :B3, attack: 0.1, attack_level: 0.1, decay: 0.2, decay_level: 1, sustain: 2.0, sustain_level: 0.8, release: 2.0, pan: 0.5
+#use_synth :hoover
+play :Fs4, amp: 0.8, cutoff: 90, attack: 0.1, attack_level: 0.1, sustain: 2.0, sustain_level: 0.8, release: 2.0, pan: 0.5
 sleep 5.0
