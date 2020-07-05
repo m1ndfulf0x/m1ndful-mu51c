47dee0c3180889e6cd5beb818c60c3af18aa9033
m1ndfulf0x <59140415+m1ndfulf0x@users.noreply.github.com>
2020-07-05 19:39:25 +0200

    modify distortion

--- terra-jingle-2.rb
+++ terra-jingle-2.rb
@@ -1,9 +1,8 @@
 # Terra Jingle Version 2
 use_bpm 75
 
-with_fx :distortion, mix: 1.0, distort: 0.9 do
-  
-  use_synth :mod_fm  #:prophet or :fm
+with_fx :distortion, mix: 0.3, distort: 0.7 do
+  use_synth :mod_fm
   play :G5
   sleep 0.5
   play :Bb4
@@ -12,10 +11,8 @@ with_fx :distortion, mix: 1.0, distort: 0.9 do
   sleep 0.5
   play :C5
   sleep 0.5
-  #sample :drum_splash_hard, finish: 0.26, release: 0.4, amp: 3.0
-  
 end
-sample :perc_snap2, amp: 3.0
+sample :perc_snap2, amp: 2.5
 sleep 0.5
 with_fx :reverb, mix: 1, room: 0.9 do
   use_synth :fm
