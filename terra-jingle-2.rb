66d762c6356a6c62b23abd2145741ce95dc0e206
m1ndfulf0x <59140415+m1ndfulf0x@users.noreply.github.com>
2020-07-05 20:16:18 +0200

    add end from 1st version as an alternative

--- terra-jingle-2.rb
+++ terra-jingle-2.rb
@@ -14,9 +14,16 @@ with_fx :distortion, mix: 0.25, distort: 0.7 do
 end
 sample :perc_snap2, amp: 2.1
 sleep 0.5
+'
 with_fx :reverb, mix: 1, room: 0.9 do
   use_synth :fm
   play :C6, amp: 4.0
   sleep 4.0
 end
-
+'
+# alternatively playing piano chord from 1st version
+use_synth :piano
+play :C4, attack: 0.1, attack_level: 0.2, decay: 0.2, decay_level: 1.8, sustain: 2.0, sustain_level: 1.5, release: 2.0
+play :C2, attack: 0.1, attack_level: 0.2, decay: 0.2, decay_level: 1.8, sustain: 2.0, sustain_level: 1.5, release: 2.0
+play :C1, attack: 0.1, attack_level: 0.2, decay: 0.2, decay_level: 1.8, sustain: 2.0, sustain_level: 1.5, release: 2.0
+play :G4, amp: 3.2, attack: 0.1, attack_level: 0.2, decay: 0.2, decay_level: 2.0, sustain: 2.0, sustain_level: 1.6, release: 2.0
